6847eccded39581935e908c41f34976132b279ff
Roland Porció <rporcio@b2international.com>
2015-08-23 09:34:53 +0200

    Updated run analysis script.

--- run_analysis.R
+++ run_analysis.R
@@ -3,36 +3,35 @@ print("Running analysis...")
 # Step 1: Merges the training and the test sets to create one data set.
 
 ## Merging X sets.
-xTrain <- read.table('./UCI HAR Dataset/train/X_train.txt')
-xTest <- read.table('./UCI HAR Dataset/test/X_test.txt')
+xTrain <- read.table("./UCI HAR Dataset/train/X_train.txt")
+xTest <- read.table("./UCI HAR Dataset/test/X_test.txt")
 x <- rbind(xTrain, xTest)
 
 ## Merging subject sets.
-subjectTrain <- read.table('./UCI HAR Dataset/train/subject_train.txt')
-subjectTest <- read.table('./UCI HAR Dataset/test/subject_test.txt')
+subjectTrain <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+subjectTest <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 subject <- rbind(subjectTrain, subjectTest)
 
 ## Merging Y sets
-yTrain <- read.table('./UCI HAR Dataset/train/y_train.txt')
-yTest <- read.table('./UCI HAR Dataset/test/y_test.txt')
+yTrain <- read.table("./UCI HAR Dataset/train/y_train.txt")
+yTest <- read.table("./UCI HAR Dataset/test/y_test.txt")
 y <- rbind(yTrain, yTest)
 
 # Step 2: Extracts only the measurements on the mean and standard deviation for each measurement.
 
 ## Filter columns from X dataset.
-features <- read.table('./UCI HAR Dataset/features.txt')
+features <- read.table("./UCI HAR Dataset/features.txt")
 featuresFiltered <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
 xFiltered <- x[, featuresFiltered]
 
 # Step 3: Use descriptive activity names to name the activities in the data set.
 
-## Formatting feature names.
+## Applying feature names to X data set.
 names(xFiltered) <- features[featuresFiltered, 2]
-names(xFiltered) <- tolower(names(xFiltered)) 
 names(xFiltered) <- gsub("\\(|\\)", "", names(xFiltered))
 
 ## Formatting activity names.
-activities <- read.table('./UCI HAR Dataset/activity_labels.txt')
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
 activities[, 2] <- tolower(as.character(activities[, 2]))
 activities[, 2] <- gsub("_", " ", activities[, 2])
 
